695d73091dabd238b647656747539f7435a8580b
adwisatya <a.dwisaty4@yahoo.com>
2015-02-07 23:55:25 +0700

    first commit. 2/3 ok

---
+++ Com.vogella.android.lifecycle.activity/app/src/main/java/com/example/adwisaty4/comvogellaandroidlifecycleactivity/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.adwisaty4.comvogellaandroidlifecycleactivity;
+
+/**
+ * Created by adwisaty4 on 2/6/2015.
+ */
+import java.util.List;
+import android.app.Application;
+
+public class MyApplication extends Application {
+    public static List<String> list;
+}
---
+++ Com.vogella.android.lifecycle.activity/app/src/main/java/com/example/adwisaty4/comvogellaandroidlifecycleactivity/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.adwisaty4.comvogellaandroidlifecycleactivity;
+
+/**
+ * Created by adwisaty4 on 2/6/2015.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SecondActivity extends TracerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
